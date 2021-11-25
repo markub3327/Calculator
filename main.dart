6ac4622d5862fbdb2bdba534684456770ca6c960
Martin Kubovcik <markub3327@gmail.com>
2021-11-25 13:22:50 +0100

    update main.dart

--- main.dart
+++ main.dart
@@ -7,6 +7,8 @@ import 'model.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 void main() {
@@ -47,15 +49,18 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+enum Providers { facebook, google, anonymous, email, register }
+
 class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   /// The future is part of the state of our widget. We should not call `initializeApp`
   /// directly inside [build].
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  RemoteConfig? remoteConfig;
 
-  AccessToken? fbAccessToken;
-  String? fbAuthorPicture;
-  String? fbAuthorName;
-  String? fbAuthorId;
+  //AccessToken? fbAccessToken;
+  //String? fbAuthorPicture;
+  //String? fbAuthorName;
+  //String? fbAuthorId;
 
   List<num> nums = [];
   List<String> opers = [];
@@ -70,7 +75,6 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   bool weatherOn = true;
   Future<http.Response>? httpWeather;
 
-  static RemoteConfig? _remoteConfig;
   final defaults = <String, dynamic>{"City": "Bratislava"};
 
   void saveMessage(Message msg) {
@@ -78,7 +82,25 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     _dbRef.push().set(msg.toJson());
   }
 
-  void initListener() {
+  void initRemoteConfig() async {
+    if (remoteConfig == null) {
+      remoteConfig = RemoteConfig.instance;
+      await remoteConfig!.setDefaults(defaults);
+      await remoteConfig!.fetch();
+
+      await remoteConfig!.setConfigSettings(RemoteConfigSettings(
+          fetchTimeout: const Duration(minutes: 5),
+          minimumFetchInterval: const Duration(hours: 12)));
+
+      city = remoteConfig!.getString("City");
+      httpWeather = http.get(Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?q=$city,SK&appid=6eb6b05fcdc850818c6238aef85610be&units=metric"));
+
+      await getWeather();
+    }
+  }
+
+  void initDatabase() {
     final DatabaseReference _dbRef =
         FirebaseDatabase.instance.reference().child("calculator");
     _dbRef
@@ -107,92 +129,132 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     });
   }
 
-  void getFacebook() async {
-    // over spravnost prihlasenia
-    if (fbAccessToken != null) {
-      final userData =
-          await FacebookAuth.instance.getUserData(fields: "id,name,picture");
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-      fbAuthorName = userData["name"];
-      fbAuthorId = userData["id"];
-      setState(() {
-        fbAuthorPicture = userData["picture"]["data"]["url"];
-        message = "Welcome, $fbAuthorName!";
-      });
-    }
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
-  void initFacebook() async {
-    if (fbAccessToken == null) {
-      final LoginResult result = await FacebookAuth.instance
-          .login(); // by default we request the email and the public profile
-      if (result.status == LoginStatus.success) {
-        fbAccessToken = result.accessToken;
+  Future<UserCredential?> registerAndSignIn(email, password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
 
-        getFacebook();
-      } else {
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
         setState(() {
-          message =
-              "Cannot connect to Facebook API: ${result.status}, ${result.message}";
+          message = 'The password provided is too weak.';        
+        });
+      } else if (e.code == 'email-already-in-use') {
+        setState(() {
+          message = 'The account already exists for that email.';        
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+
+    return null;
+  }
+
+  void Logout() async {
+    await FirebaseAuth.instance.signOut();
+
+    setState(() {
+      message = "You're logged out!";
+    });
+  }
+
+  Future<UserCredential?> signInWithEmail(email, password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        setState(() {
+          message = 'No user found for that email.';        
+        });
+      } else if (e.code == 'wrong-password') {
+        setState(() {
+          message = 'Wrong password provided for that user.';        
         });
       }
     }
+
+    return null;
   }
 
-  @override
-  void initState() {
-    super.initState();
-
-    WidgetsBinding.instance!.addObserver(this);
-
-    initListener();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-
-    WidgetsBinding.instance!.removeObserver(this);
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    super.didChangeAppLifecycleState(state);
-
-    if (state == AppLifecycleState.paused) {
-      // isiel do pozadia
+  void initLogin(Providers provider) async {
+    if (provider == Providers.email) {
+      var userCredential =
+          await signInWithEmail("markub3327@test.sk", "android");
+    } else if (provider == Providers.google) {
+      var userCredential = await signInWithGoogle();
+    } else if (provider == Providers.anonymous) {
+      var userCredential = await FirebaseAuth.instance.signInAnonymously();
+    } else if (provider == Providers.register) {
+      var userCredential =
+          await registerAndSignIn("markub3327@test.sk", "android");
     }
-    if (state == AppLifecycleState.resumed) {
-      // prichadza do popredia
-      if (_remoteConfig != null) {
-        _remoteConfig!.activate();
 
-        city = _remoteConfig!.getString("City");
-        httpWeather = http.get(Uri.parse(
-            "https://api.openweathermap.org/data/2.5/weather?q=$city,SK&appid=6eb6b05fcdc850818c6238aef85610be&units=metric"));
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
 
-        getWeather();
-        getFacebook();
+      } else {
+        setState(() {
+          if (user.displayName != null)
+            message = "Welcome, ${user.displayName}!";
+          else if (user.email != null)
+            message = "Welcome, ${user.email}!";
+          else
+            message = "Welcome, Anonymous";
+        });
       }
-    }
-  }
+    });
 
-  initRemoteConfig() async {
-    if (_remoteConfig == null) {
-      _remoteConfig = RemoteConfig.instance;
-      await _remoteConfig!.setDefaults(defaults);
-      await _remoteConfig!.fetch();
+    FirebaseAuth.instance.idTokenChanges().listen((User? user) {
+      if (user == null) {
 
-      await _remoteConfig!.setConfigSettings(RemoteConfigSettings(
-          fetchTimeout: const Duration(minutes: 5),
-          minimumFetchInterval: const Duration(hours: 12)));
+      } else {
+        setState(() {
+          if (user.displayName != null)
+            message = "Welcome, ${user.displayName}!";
+          else if (user.email != null)
+            message = "Welcome, ${user.email}!";
+          else
+            message = "Welcome, Anonymous";
+        });
+      }
+    });
 
-      city = _remoteConfig!.getString("City");
-      httpWeather = http.get(Uri.parse(
-          "https://api.openweathermap.org/data/2.5/weather?q=$city,SK&appid=6eb6b05fcdc850818c6238aef85610be&units=metric"));
+    FirebaseAuth.instance.userChanges().listen((User? user) {
+      if (user == null) {
 
-      await getWeather();
-    }
+      } else {
+        setState(() {
+          if (user.displayName != null)
+            message = "Welcome, ${user.displayName}!";
+          else if (user.email != null)
+            message = "Welcome, ${user.email}!";
+          else
+            message = "Welcome, Anonymous";
+        });
+      }
+    });
   }
 
   Future<void> getWeather() async {
@@ -211,6 +273,38 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
       setState(() {
         input = "Cannot read weather !";
       });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance!.addObserver(this);
+
+    initDatabase();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    WidgetsBinding.instance!.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    if (state == AppLifecycleState.paused) {}
+    if (state == AppLifecycleState.resumed) {
+      RemoteConfig.instance.activate();
+
+      city = RemoteConfig.instance.getString("City");
+      httpWeather = http.get(Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?q=$city,SK&appid=6eb6b05fcdc850818c6238aef85610be&units=metric"));
+
+      getWeather();
     }
   }
 
@@ -449,8 +543,10 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
         process(opers.last);
       }
 
+      var name = (FirebaseAuth.instance.currentUser != null) ? FirebaseAuth.instance.currentUser!.displayName : "null";
+      var id = (FirebaseAuth.instance.currentUser != null) ? FirebaseAuth.instance.currentUser!.uid : "null";
       saveMessage(Message(
-          nums, opers, result, DateTime.now(), fbAuthorId, fbAuthorName));
+          nums, opers, result, DateTime.now(), id, name));
     }
   }
 
@@ -486,12 +582,10 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
 
         // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
-          // init Remote config
           initRemoteConfig();
-          initFacebook();
 
           Timer.periodic(const Duration(minutes: 5), (_) async {
-            if (_remoteConfig != null && weatherOn) {
+            if (weatherOn) {
               getWeather();
             }
           });
@@ -507,7 +601,9 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
                     margin: const EdgeInsets.all(15),
                     child: Text(input, style: const TextStyle(fontSize: 25))),
                 Row(children: [
-                  if (fbAuthorPicture != null) Image.network(fbAuthorPicture!),
+                  if (FirebaseAuth.instance.currentUser != null && FirebaseAuth.instance.currentUser!.photoURL != null)                  
+                    Image.network(FirebaseAuth.instance.currentUser!.photoURL!,
+                        width: 50),
                   Container(
                       margin: const EdgeInsets.all(15),
                       child:
@@ -674,6 +770,57 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
                         icon: const Icon(Icons.backspace),
                       ),
                     ])),
+                Expanded(
+                    child: Row(
+                        //ROW 7
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: (FirebaseAuth.instance.currentUser == null)
+                            ? [
+                                Text('Login with'),
+                                TextButton(
+                                  onPressed: () => initLogin(Providers.email),
+                                  child: const Text("Email",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                        color: Colors.black,
+                                      )),
+                                ),
+                                TextButton(
+                                  onPressed: () => initLogin(Providers.google),
+                                  child: const Text("Google",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                        color: Colors.black,
+                                      )),
+                                ),
+                                TextButton(
+                                  onPressed: () =>
+                                      initLogin(Providers.anonymous),
+                                  child: const Text("Anonymous",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                        color: Colors.black,
+                                      )),
+                                ),
+                                TextButton(
+                                  onPressed: () => initLogin(Providers.register),
+                                  child: const Text("Register",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                        color: Colors.black,
+                                      )),
+                                )
+                              ]
+                            : [
+                                TextButton(
+                                  onPressed: () => Logout(),
+                                  child: const Text("Logout",
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                        color: Colors.black,
+                                      )),
+                                )
+                              ])),
               ]));
         }
 
